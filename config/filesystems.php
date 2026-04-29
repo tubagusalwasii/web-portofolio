cd824fb03d5232e333ae9329c85284ac9d467a10
tubagusalwasii <tubagusalesi@gmail.com>
2026-04-29 23:04:01 +0700

    fix: correct cloudinary disk keys in filesystems config

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,6 +62,9 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
+            'cloud' => env('CLOUDINARY_CLOUD_NAME'),
+            'key' => env('CLOUDINARY_API_KEY'),
+            'secret' => env('CLOUDINARY_API_SECRET'),
         ],
 
     ],
